533fabb825a3e4b7ba0a7b964f4bce11db35be57
leonid.umanskiy <leonid.umanskiy@aofl.com>
2020-09-30 18:12:51 -0700

    Bump up version to 0.2.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     url='https://github.com/AgeOfLearning/upackage',
-    version='0.2.0',
+    version='0.2.1',
     zip_safe=False,
 )
