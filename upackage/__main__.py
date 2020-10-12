dc25b8322b69d01c5d5616a5ab872cfdc754d9a8
leonid.umanskiy <leonid.umanskiy@aofl.com>
2020-10-12 11:05:47 -0700

    Fixing cli

--- upackage/__main__.py
+++ upackage/__main__.py
@@ -3,12 +3,12 @@ from upackage.upackage import UPackage
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("usage: upackage path/to/content output.unitypackage")
         exit(1)
 
-    package_root = sys.argv[0]
-    output_path = sys.argv[1]
+    package_root = sys.argv[1]
+    output_path = sys.argv[2]
 
     UPackage.preprocess_assets(package_root)
     UPackage.generate_package(package_root, output_path)
